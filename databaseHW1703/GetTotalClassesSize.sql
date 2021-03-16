a1e5a255cd3af4d1f399474057669954d84609b8
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2021-03-16 12:29:15 +0700

    Database HW
    StoredProcedure GetTotalClassesSize.sql
    
    ERD version 1 and 2 (not finalized)
    
    Relational Schema (not finalized)

---
+++ databaseHW1703/GetTotalClassesSize.sql
@@ -0,0 +1,46 @@
+/*
+   Get sport centers from a city identified by the cityId
+*/
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS GetTotalClassesSize  //
+CREATE PROCEDURE GetTotalClassesSize (
+	IN inAYearId INT,
+    IN inSemesterId INT,
+    IN inFacultyId INT,
+    IN inProgramId VARCHAR(5),
+    IN inModuleId INT,
+    IN inLecturerId INT,
+    IN inClassId INT,
+    OUT statusCode INT
+)
+BEGIN
+	IF inAYearId NOT IN (SELECT AYearId FROM AcademicYear) 
+    AND inSemesterId NOT IN (SELECT SemesterId FROM Semester)
+    AND inFacultyId NOT IN (SELECT FacultyId FROM Faculty) 
+    AND inProgramId NOT IN (SELECT ProgramId FROM Program) 
+    AND inModuleId NOT IN (SELECT ModuleId FROM Module) 
+    AND inLecturerId NOT IN (SELECT LecturerId FROM Lecturer) 
+    AND inClassId NOT IN (SELECT ClassId FROM Class) THEN
+		SET statusCode = 405; -- invalid code
+	ELSE
+		SET statusCode = 200;
+		
+        SELECT SUM(Size) AS TotalClassesSize
+		FROM Class
+		NATURAL JOIN Lecturer 
+        NATURAL JOIN Module
+        NATURAL JOIN Program
+        NATURAL JOIN Faculty
+        NATURAL JOIN Semester
+        NATURAL JOIN AcademicYear
+		WHERE LecturerId = inLecturerId 
+        AND SemesterId=inSemesterId 
+        AND ModuleId=inModuleId
+		AND ProgramId=inProgramId
+        AND FacultyId=inFacultyId
+		AND AYearId=inAYearId;
+	END IF;
+END//
+
+DELIMITER ;
