556df4d4e50e0a21194272346ec01dba04b762b6
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2021-03-20 23:35:25 +0700

    Update 01_tables_def.sql

--- database/DatabaseSchema/01_tables_def.sql
+++ database/DatabaseSchema/01_tables_def.sql
@@ -1,16 +1,4 @@
-DROP TABLE IF EXISTS Questionnaire;
-DROP TABLE IF EXISTS Teaching;
-DROP TABLE IF EXISTS Lecturer;
-DROP TABLE IF EXISTS Class;
-DROP TABLE IF EXISTS ModuleInProgram;
-DROP TABLE IF EXISTS ProgramInFaculty;
-DROP TABLE IF EXISTS FacultyInAcademicYear;
-DROP TABLE IF EXISTS SemesterInAcademicYear;
-DROP TABLE IF EXISTS Module;
-DROP TABLE IF EXISTS Program;
-DROP TABLE IF EXISTS Faculty;
-DROP TABLE IF EXISTS Semester;
-DROP TABLE IF EXISTS AcademicYear;
+
 
 CREATE TABLE IF NOT EXISTS AcademicYear (
     AYearId VARCHAR(10),
@@ -29,9 +17,9 @@ CREATE TABLE IF NOT EXISTS SemesterInAcademicYear (
     AYearId VARCHAR(10),
     PRIMARY KEY(SemesterId,AYearId),
 	FOREIGN KEY (SemesterId)
-	REFERENCES Semester (SemesterId),
+		REFERENCES Semester (SemesterId),
 	FOREIGN KEY (AYearId)
-	REFERENCES AcademicYear (AYearId)
+		REFERENCES AcademicYear (AYearId)
 );
 
 CREATE TABLE IF NOT EXISTS Faculty (
@@ -45,9 +33,9 @@ CREATE TABLE IF NOT EXISTS FacultyInAcademicYear (
     AYearId VARCHAR(10),
     PRIMARY KEY(FacultyId,AYearId),
 	FOREIGN KEY (FacultyId)
-	REFERENCES Faculty (FacultyId),
+		REFERENCES Faculty (FacultyId),
 	FOREIGN KEY (AYearId)
-	REFERENCES AcademicYear (AYearId)
+		REFERENCES AcademicYear (AYearId)
 );
     
 CREATE TABLE IF NOT EXISTS Program (
@@ -57,14 +45,17 @@ CREATE TABLE IF NOT EXISTS Program (
     PRIMARY KEY (ProgramId)
 );
 
-CREATE TABLE IF NOT EXISTS ProgramInFaculty (
+CREATE TABLE IF NOT EXISTS ProgramInFacultyInAcademicYear (
 	ProgramId VARCHAR(10),
 	FacultyId VARCHAR(10),
-    PRIMARY KEY(ProgramId,FacultyId),
+    AYearId VARCHAR(10),
+    PRIMARY KEY(ProgramId,FacultyId,AYearId),
 	FOREIGN KEY (ProgramId)
-	REFERENCES Program (ProgramId),
+		REFERENCES Program (ProgramId),
 	FOREIGN KEY (FacultyId)
-	REFERENCES Faculty (FacultyId)
+		REFERENCES Faculty (FacultyId),
+	FOREIGN KEY (AYearId)
+		REFERENCES AcademicYear (AYearId)
 );
 
 CREATE TABLE IF NOT EXISTS Module (
@@ -74,32 +65,39 @@ CREATE TABLE IF NOT EXISTS Module (
     PRIMARY KEY (ModuleId)
 );
 
-CREATE TABLE IF NOT EXISTS ModuleInProgram (
+CREATE TABLE IF NOT EXISTS ModuleInProgramInAcademicYear (
 	ModuleId VARCHAR(10),
 	ProgramId VARCHAR(10),
-    PRIMARY KEY(ModuleId,ProgramId),
+    AYearId VARCHAR(10),
+    
+    PRIMARY KEY(ModuleId,ProgramId,AYearId),
 	FOREIGN KEY (ModuleId)
-	REFERENCES Module (ModuleId),
+		REFERENCES Module (ModuleId),
 	FOREIGN KEY (ProgramId)
-	REFERENCES Program (ProgramId)
+		REFERENCES Program (ProgramId),
+    FOREIGN KEY (AYearId)
+		REFERENCES AcademicYear (AYearId)
 );
 
 CREATE TABLE IF NOT EXISTS Class (
     ClassId VARCHAR(10),
     Size INT NOT NULL,
-    SemesterId VARCHAR(10) NOT NULL,
-    ModuleId VARCHAR(10) NOT NULL,
 
-    PRIMARY KEY (ClassId),
-	FOREIGN KEY (SemesterId)
+    PRIMARY KEY (ClassId)
+);
+
+CREATE TABLE IF NOT EXISTS ClassInSemesterModuleAcademicYear (
+	ClassId VARCHAR(10),
+    SemesterId VARCHAR(10),
+    ModuleId VARCHAR(10),
+    AYearId VARCHAR(10),
+    PRIMARY KEY(ClassId,SemesterId,ModuleId,AYearId),
+    FOREIGN KEY (SemesterId)
 		REFERENCES Semester (SemesterId),
 	FOREIGN KEY (ModuleId)
 		REFERENCES Module (ModuleId),
-    CONSTRAINT ClassInModule
-        UNIQUE (ModuleId, ClassId),
-	CONSTRAINT ClassInSemester
-        UNIQUE (SemesterId, ClassId)
-        
+	FOREIGN KEY (AYearId)
+		REFERENCES AcademicYear (AYearId)
 );
 
 CREATE TABLE IF NOT EXISTS Lecturer (
