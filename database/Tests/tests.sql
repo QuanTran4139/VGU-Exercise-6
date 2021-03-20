a238f20f0c78dc814fc140bc1ddb1dee0a309bd4
NguyenGiaKimThien <71683111+NguyenGiaKimThien@users.noreply.github.com>
2021-03-20 19:55:55 +0700

    Update tests.sql

--- database/Tests/tests.sql
+++ database/Tests/tests.sql
@@ -1,3 +1,8 @@
 CALL GetTotalClassesSize('2001','WS01','CENG01','EE01','EECa1','1',@statusCode);
 
 CALL GetTotalClassesSize('2001','WS01','CENG01','CS01','ComCa1','1',@statusCode);
+
+// Modified for current procedures + tests (20/03/21)
+CALL GetTotalClassesSize('BOB', 'Calculus', 'Electrical Engineering', 'Computer And Engineering', 'WinterSemester', '2001', @statusCode);
+// Expected code results - 130
+
