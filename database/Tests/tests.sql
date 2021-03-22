c0a3e7582770e90de1d72c36ee2a7d86e8602296
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2021-03-22 17:58:04 +0700

    Update tests.sql

--- database/Tests/tests.sql
+++ database/Tests/tests.sql
@@ -1,5 +1,5 @@
 # Modified for current procedures + tests (22/03/21)
-CALL GetTotalClassesSize(2002,'WS01' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', @statusCode);
+CALL GetTotalClassesSize(2005,'WS01' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', @statusCode);
 # Expected code results - 401
 
 CALL GetTotalClassesSize(2001,'WS' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', @statusCode);
