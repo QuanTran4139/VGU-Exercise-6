bbfd129ffef9d4aed8fa02c0d230bae99375273c
Hai Vu <57252568+duchai9109@users.noreply.github.com>
2021-03-23 11:34:59 +0700

    Changed QuestionnaireResource.java
    statusCode can now be able to print in web browser

--- src/test/java/QuestionnaireResource.java
+++ src/test/java/QuestionnaireResource.java
@@ -19,60 +19,26 @@ public class QuestionnaireResource {
 	public Response getProgramsCount() throws SQLException, NamingException   {
 		
 		Connection db = Configuration.getAcademiaConnection();
+		int statusCode;
+		
 		try {
 			
 //			PreparedStatement st = db.prepareC(
 //					"CALL createAcademicYear('1', @statusCode);"
 //					);
 //			ResultSet rs = st.executeQuery();
-//			
 			
-			String name = "";
-//			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2005,'WS01' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', ?);");){
-//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-//				cstmt.execute();
-//				System.out.println(cstmt.getInt(1));
-//			}
-//			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2001,'WS' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', ?);");){
-//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-//				cstmt.execute();
-//				System.out.println(cstmt.getInt(1));
-//			}
-//			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2001,'WS01' ,'Nursery', 'Electrical Engineering' , 'Calculus','1', ?);");){
-//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-//				cstmt.execute();
-//				System.out.println(cstmt.getInt(1));
-//			}
-//			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2001,'WS01' ,'Computer And Engineering', 'Pharmacy' , 'Calculus','1', ?);");){
-//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-//				cstmt.execute();
-//				System.out.println(cstmt.getInt(1));
-//			}
-//			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2001,'WS01' ,'Computer And Engineering', 'Electrical Engineering' , 'Wizardry','1', ?);");){
-//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-//				cstmt.execute();
-//				System.out.println(cstmt.getInt(1));
-//			}
-			try (CallableStatement cstmt = db.prepareCall("CALL createAcademicYear('1', ?);");){
+			try (CallableStatement cstmt = db.prepareCall("CALL GetTotalClassesSize(2005,'WS01' ,'Computer And Engineering', 'Electrical Engineering' , 'Calculus','1', ?);");){
 				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
 				cstmt.execute();
-				System.out.println(cstmt.getInt(1));
+				statusCode = cstmt.getInt(1);
 			}
-			try (CallableStatement cstmt = db.prepareCall("CALL createAcademicYear('1', ?);");){
-				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-				cstmt.execute();
-				System.out.println(cstmt.getInt(1));
-			}
-			try (CallableStatement cstmt = db.prepareCall("CALL createAcademicYear('1', ?);");){
-				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-				cstmt.execute();
-				System.out.println(cstmt.getInt(1));
-			}
-			try (CallableStatement cstmt = db.prepareCall("CALL createAcademicYear('@', ?);");){
-				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
-				cstmt.execute();
-				System.out.println(cstmt.getInt(1));
-			}
+//			try (CallableStatement cstmt = db.prepareCall("CALL createAcademicYear('1', ?);");){
+//				cstmt.registerOutParameter(1,java.sql.Types.INTEGER);
+//				cstmt.execute();
+//				statusCode = cstmt.getInt(1);
+//			}
+
 			
 			
 //			while (rs.next()) {
@@ -82,10 +48,10 @@ public class QuestionnaireResource {
 			
 			
 		
-//		JsonObject entry = Json.createObjectBuilder()
-//				.add("ClassId", name).build();
-//				
-		return null;// Response.ok().entity(entry.toString()).build();
+		JsonObject entry = Json.createObjectBuilder()
+				.add("status", statusCode).build();
+				
+		return Response.ok().entity(entry.toString()).build();
 		
 		}
 		finally {
