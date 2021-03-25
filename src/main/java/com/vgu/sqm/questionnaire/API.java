c641613a28e7b338d7dd85570a30907c328c17a8
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2021-03-25 19:15:28 +0700

    Removed .settings; Renamed api -> API

--- src/main/java/com/vgu/sqm/questionnaire/API.java
+++ src/main/java/com/vgu/sqm/questionnaire/API.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class api
  */
 @WebServlet("/api")
-public class api extends HttpServlet {
+public class API extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public api() {
+    public API() {
         super();
         // TODO Auto-generated constructor stub
     }
