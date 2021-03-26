d4ee0cb7e92d9a08bcccadb8dd8a6a5b776bbf8c
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2021-03-26 18:35:10 +0700

    Added JSON dependency; Create Module class

--- src/main/java/com/vgu/sqm/questionnaire/Module.java
+++ src/main/java/com/vgu/sqm/questionnaire/Module.java
@@ -1,40 +1,37 @@
 package com.vgu.sqm.questionnaire;
 
 import java.io.IOException;
+import org.json.simple.JSONObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class api
- */
-@WebServlet("/api")
-public class API extends HttpServlet {
+@WebServlet("/api/module")
+public class Module extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+    private String ModuleID;
+    private String ModuleName;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public API() {
-        super();
-        // TODO Auto-generated constructor stub
+    public Module(String ModuleID, String ModuleName) {
+        this.ModuleID = ModuleID;
+        this.ModuleName = ModuleName;
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    private JSONObject exportJsonData(){
+        JSONObject module = new JSONObject();
+        module.put("ID", this.ModuleID);
+        module.put("name", this.ModuleName);
+        return module;
+    }
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.setContentType("application/json");
+		response.getWriter().print(exportJsonData());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
