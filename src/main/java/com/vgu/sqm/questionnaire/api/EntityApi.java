037cacecc3cc5dec6f70cd2c7f5d1c8cd41e9b1a
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2021-03-27 17:32:01 +0700

    Improved error handling for "action" parameter

--- src/main/java/com/vgu/sqm/questionnaire/api/EntityApi.java
+++ src/main/java/com/vgu/sqm/questionnaire/api/EntityApi.java
@@ -1,8 +1,8 @@
 package com.vgu.sqm.questionnaire.api;
 
 import com.vgu.sqm.questionnaire.core.Entity;
-import java.util.ArrayList;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -31,9 +31,13 @@ public abstract class EntityApi extends HttpServlet {
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_OK);
             response.getWriter().print(EntitiesToJson(dumpEntities()));
+        } else if (action.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().print("You need to supply an 'action'");
         } else {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getWriter().print("NOPE");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            String message = String.format("Action '%s' is not supported", action);
+            response.getWriter().print(message);
         }
     }
 
