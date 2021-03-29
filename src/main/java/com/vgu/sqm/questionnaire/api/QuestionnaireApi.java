c13ab685c2f650e4b9c3470f9a4dc3ba5a555bea
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2021-03-29 16:28:54 +0700

    Updated Questionnaire classes to implement the Questionnaire item

--- src/main/java/com/vgu/sqm/questionnaire/api/QuestionnaireApi.java
+++ src/main/java/com/vgu/sqm/questionnaire/api/QuestionnaireApi.java
@@ -3,7 +3,6 @@ package com.vgu.sqm.questionnaire.api;
 import com.vgu.sqm.questionnaire.database.Database;
 import com.vgu.sqm.questionnaire.resource.Questionnaire;
 import com.vgu.sqm.questionnaire.resource.Resource;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -28,15 +27,18 @@ public class QuestionnaireApi extends ResourceApi {
             CallableStatement st = db.prepareCall("CALL DumpQuestionnaire()");
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
-                String lId = rs.getString(1); //Attribute name LecturerID
-                String cId = rs.getString(2); //Attribute name ClassID
-                String content = rs.getString(3); //Attribute name Content
+                String lId = rs.getString(1); // Attribute name LecturerID
+                String cId = rs.getString(2); // Attribute name ClassID
+                String content = rs.getString(3); // Attribute name Content
 
-                if (content == null){
+                if (content == null) {
                     content = "";
                 }
 
-                resources.add(new Questionnaire(lId, cId, content));
+                // TODO get actual data for this
+                int[] answers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+
+                resources.add(new Questionnaire(lId, cId, 'N', answers, content));
             }
             LOGGER.log(Level.INFO, "Getting info from database successfully.");
             db.close();
--- src/main/java/com/vgu/sqm/questionnaire/resource/Questionnaire.java
+++ src/main/java/com/vgu/sqm/questionnaire/resource/Questionnaire.java
@@ -1,25 +1,42 @@
 package com.vgu.sqm.questionnaire.resource;
 
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
 public class Questionnaire implements Resource {
     private String LecturerID;
     private String ClassID;
-    private String Content;
+    private char gender;
+    private int[] answers;
+    private String comment;
 
-    public Questionnaire(String LecturerID, String ClassID, String Content) {
+    private JsonArray intArrayToJsonArray(int[] array) {
+        JsonArrayBuilder builder = Json.createArrayBuilder();
+        for (int a : this.answers){
+            builder.add(a);
+        }
+        return builder.build();
+    }
+
+    public Questionnaire(
+        String LecturerID, String ClassID, char gender, int[] answers, String comment) {
         this.LecturerID = LecturerID;
         this.ClassID = ClassID;
-        this.Content = Content;
+        this.gender = gender;
+        this.answers = answers;
+        this.comment = comment;
     }
 
     public JsonObject exportResourceJson() {
         JsonObjectBuilder builder = Json.createObjectBuilder();
         builder.add("LecturerID", this.LecturerID);
         builder.add("ClassID", this.ClassID);
-        builder.add("Content", this.Content);
+        builder.add("gender", this.gender);
+        builder.add("answers", intArrayToJsonArray(this.answers));
+        builder.add("comment", this.comment);
         JsonObject obj = builder.build();
         return obj;
     }
