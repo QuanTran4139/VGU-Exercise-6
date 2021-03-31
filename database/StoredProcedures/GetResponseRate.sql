6f7a07c5055299ccdffeebdb67a9d591b0c695ba
NguyenGiaKimThien <71683111+NguyenGiaKimThien@users.noreply.github.com>
2021-03-31 10:39:36 +0700

    Added the GetResponseRate procedure

---
+++ database/StoredProcedures/GetResponseRate.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+DROP PROCEDURE IF EXISTS ResponseRate //
+CREATE PROCEDURE ResponseRate(
+IN inLecturerId VARCHAR(10),
+IN inClassId VARCHAR(10),
+OUT statusCode INT
+)
+BEGIN
+	CASE
+		WHEN inCLassId NOT IN (SELECT ClassId FROM Class) THEN SET statusCode = 407; -- wrong ClassID
+        WHEN inLecturerId NOT IN (SELECT LecturerId FROM Lecturer) THEN SET statusCode = 416; -- wrong Lecturer
+        ELSE SET statusCode = 200; -- Success
+        SELECT ClassId, LecturerId, COUNT(QuestionnaireId) / (SELECT Size FROM Class WHERE ClassId = QuestionnaireId) * 100 AS Response_Rate
+        FROM Questionnaire
+        WHERE ClassId = inClassId
+		AND	  LecturerId = inLecturerId
+        GROUP BY ClassId;
+	END CASE;
+END//
+
+DELIMITER ;
