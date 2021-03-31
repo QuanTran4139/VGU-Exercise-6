9e428069e91eaa54bd55a6268cccc5ea5af83268
Noe315 <34193837+Noe315@users.noreply.github.com>
2021-03-31 09:43:08 +0700

    Delete DumpFacultyInAcademicYear.js

--- www/tables/js/DumpFacultyInAcademicYear.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-	$.get("/Questionnaire/api/facultyInAcademicYear?action=dump", function(data, status)
-	{
-		//alert("Data: " + data + " Status: " + status);
-		console.log(JSON.stringify(data));
-	});
-});
