b74b1cf3c4b8809dc7221158292bd8450c86f57f
Noe315 <34193837+Noe315@users.noreply.github.com>
2021-03-31 09:41:55 +0700

    Delete DumpTeaching.js

--- www/tables/js/DumpTeaching.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-	$.get("/Questionnaire/api/teaching?action=dump", function(data, status)
-	{
-		//alert("Data: " + data + " Status: " + status);
-		console.log(JSON.stringify(data));
-	});
-});
